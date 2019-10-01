2b63629ff7212062ffb04c715e1712453161c156
Mikhail Andreev <mikhaila@123software.ru>
2019-10-01 16:48:26 +0300

    refactor: elsint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,19 @@ const overrides = require('@xcritical/eslint-plugin-xc-front-lint/overrides/type
 
 module.exports = {
   extends: ['plugin:@xcritical/eslint-plugin-xc-front-lint/base'],
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [
+          ['@xcritical/xc-theme', './packages/xc-theme/src'],
+        ],
+        extensions: ['.ts', '.js', '.jsx', '.json']
+      }
+    },
+  },
+  "rules": {
+    "react/jsx-filename-extension": [1, { "extensions": [".tsx", ".jsx"] }],
+    "no-console": [1, { allow: ["error"] }]
+  },
   overrides,
 };
