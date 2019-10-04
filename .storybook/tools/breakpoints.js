815a31a2dd250c750dac38ca9cd513f623f67b83
Mikhail Andreev <tundraray@outlook.com>
2019-10-05 00:14:52 +0300

    ci: added storybook generator

---
+++ .storybook/tools/breakpoints.js
@@ -0,0 +1,42 @@
+export const breakpoints = {
+  xSmall: {
+    device: 'iPhone 5/SE',
+    height: 568,
+    width: 320,
+  },
+  small: {
+    device: 'iPhone 6/7',
+    height: 667,
+    width: 375,
+  },
+  smallMedium: {
+    device: 'iPhone 6/7 Plus',
+    height: 736,
+    width: 414,
+  },
+  medium: {
+    device: 'iPad Pro 10.5-inch',
+    height: 834,
+    width: 1112,
+  },
+  largeMedium: {
+    device: 'iPad Retina/Air/Pro 9.7"',
+    height: 1024,
+    width: 768,
+  },
+  large: {
+    device: 'iPad Pro 12.9"',
+    height: 1366,
+    width: 1024,
+  },
+  xLarge: {
+    device: 'MacBook Air 11"',
+    height: 768,
+    width: 1366,
+  },
+  giant: {
+    device: 'Desktop',
+    height: 1200,
+    width: 1850,
+  },
+};
