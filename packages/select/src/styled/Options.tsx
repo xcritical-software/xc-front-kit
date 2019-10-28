b84a5a36f2f9dec69465e044323485a94b8a1484
Mikhail Andreev <tundraray@outlook.com>
2019-10-28 17:29:32 +0300

    feat(select): add prefix and postfix element into value object

---
+++ packages/select/src/styled/Options.tsx
@@ -0,0 +1,122 @@
+import React, { CSSProperties } from 'react';
+import { IThemeNamespace } from '@xcritical/theme';
+import styled from 'styled-components';
+import { FormatOptionLabelContext, FormatOptionLabelMeta } from 'react-select/src/Select';
+import { getPaddingStyles, getCustomStyles } from '../utils';
+import {
+  IOptionProps, SelectTheme, IPrefixProps, IOptionItem,
+} from '../interfaces';
+
+
+const LabelPrefixPostfixBase = styled.span<IPrefixProps>`
+direction: inherit;
+align-items: center;
+display: flex;
+flex-shrink: 0;
+`;
+
+
+export const LabelPrefix = styled(LabelPrefixPostfixBase)`
+margin-${({ isRTL }) => (isRTL ? 'left' : 'right')}: ${(
+  { theme, appearance = 'default', baseAppearance = 'default' },
+) => getCustomStyles(theme, appearance, baseAppearance)('labelText', 'prefixSpacing')}px;
+`;
+
+export const LabelPostfix = styled(LabelPrefixPostfixBase)`
+margin-${({ isRTL }) => (isRTL ? 'right' : 'left')}: ${(
+  { theme, appearance = 'default', baseAppearance = 'default' },
+) => getCustomStyles(theme, appearance, baseAppearance)('labelText', 'prefixSpacing')}px;
+`;
+
+
+const labelCSS = (context: FormatOptionLabelContext): CSSProperties => ({
+  display: 'flex',
+  alignItems: 'center',
+  position: 'relative',
+  top: context === 'value' ? '1px' : '0',
+});
+
+const textCSS = (
+  isRTL: boolean,
+  theme: IThemeNamespace<SelectTheme>,
+  appearance: string,
+  baseAppearance: string,
+): CSSProperties => {
+  let paddingRight;
+  let paddingLeft;
+
+  const padding = getPaddingStyles(theme, appearance, baseAppearance)('labelText');
+
+  if (isRTL) {
+    paddingRight = (padding && padding.paddingRight) || 0;
+  } else {
+    paddingLeft = (padding && padding.paddingLeft) || 0;
+  }
+
+  return {
+    paddingLeft,
+    paddingRight,
+  };
+};
+
+const Option: React.FC<IOptionProps> = ({
+  context,
+  prefix,
+  postfix,
+  children,
+  isRTL,
+  theme = {},
+  appearance = 'default',
+  baseAppearance = 'default',
+}) => (
+  <div style={ labelCSS(context) }>
+    { !!prefix && (
+      <LabelPrefix
+        theme={ theme }
+        appearance={ appearance }
+        baseAppearance={ baseAppearance }
+        isRTL={ isRTL }
+      >
+        { prefix }
+      </LabelPrefix>
+    ) }
+
+    <span style={ textCSS(isRTL, theme, appearance, baseAppearance) }>{ children }</span>
+
+    { !!postfix && (
+      <LabelPostfix
+        theme={ theme }
+        appearance={ appearance }
+        baseAppearance={ baseAppearance }
+        isRTL={ isRTL }
+      >
+        { postfix }
+      </LabelPostfix>
+    ) }
+  </div>
+);
+
+export const getFormatOptionLabel = (
+  theme: IThemeNamespace<SelectTheme>,
+  appearance: string,
+  baseAppearance: string,
+  isRTL: boolean,
+) => (
+  opt: IOptionItem,
+  { context, inputValue }: FormatOptionLabelMeta<IOptionProps>,
+) => {
+  console.log(opt, inputValue);
+  return (
+    <Option
+      theme={ theme }
+      appearance={ appearance }
+      baseAppearance={ baseAppearance }
+      prefix={ opt.prefix }
+      postfix={ opt.postfix }
+      context={ context }
+      isRTL={ isRTL }
+    >
+      { opt.label }
+    </Option>
+  );
+};
