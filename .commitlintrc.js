19ee71516eda33d5c114ce5d0e5598099c3de4d4
Olga Mityukhina <olgam@xcritical.software>
2020-04-24 20:05:39 +0300

    refactor: add new type for commitlint

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert', 'perf', 'bump', 'ci', 'build']],
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert', 'perf', 'bump', 'ci', 'build', 'chore', 'types']],
     'subject-case': [0],
     'header-max-length': [0],
     'subject-full-stop': [0],
