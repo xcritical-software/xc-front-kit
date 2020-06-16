23661bc2be3fcc34cd650df7449361c3eddbb8c6
Mikhail Andreev <tundraray@outlook.com>
2020-06-16 22:55:20 +0300

    fixup! ci: change deployment scripts

--- copy-to-publish.sh
+++ copy-to-publish.sh
@@ -3,13 +3,13 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PUBLISH_DIR="$CURRENT_DIR/.publish"
 rm -rf $PUBLISH_DIR/*
 
-for workspaces in $(yarn boltify ws --json  --since HEAD~1 | grep location  | awk -F: '{ print $2 }' | sed 's/[",]//g')
+for workspaces in $(yarn boltify ws --json --since HEAD~1 | grep location  | awk -F: '{ print $2 }' | sed 's/[",]//g')
 do
 
 PROJECT_NAME=`basename $CURRENT_DIR/$workspaces`
 PUBLISH_PROJECT_DIR="$CURRENT_DIR/.publish/$PROJECT_NAME"
 SOURCE_PROJECT_DIR="$CURRENT_DIR/$workspaces"
-
+echo $PROJECT_NAME;
 if [[ -e "$SOURCE_PROJECT_DIR/dist" ]]; then
 
   if [[ ! -e $PUBLISH_PROJECT_DIR ]]; then
