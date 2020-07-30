76ef6086e641c171abd48bcb96f5630c81c18f32
Mikhail Andreev <tundraray@outlook.com>
2020-07-30 12:55:28 +0300

    build: remove duplicate settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,15 +17,11 @@ module.exports = {
     "react/jsx-filename-extension": [1, {
       "extensions": [".tsx", ".jsx"]
     }],
-    "react/prop-types": 0,
     "no-console": [1, {
       allow: ["error"]
     }],
     "react/prop-types": 0,
     "@typescript-eslint/no-empty-function": 0,
-    "@typescript-eslint/no-loss-of-precision": 0,
-    "@typescript-eslint/ban-tslint-comment": 0,
-    "@typescript-eslint/no-confusing-non-null-assertion": 0,
     "@typescript-eslint/no-unnecessary-condition": "warn",
     "@typescript-eslint/ban-tslint-comment": 0,
     "@typescript-eslint/no-confusing-non-null-assertion": 0,
