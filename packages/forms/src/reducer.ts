02ee9e45f7c0e1ae16923c77315d91033ac7fc4d
Yaroslav Moiseev <yaroslavmo@wizardsdev.com>
2020-08-12 13:56:08 +0300

    fix(forms): Fix formSelector return logic when data not found.

--- packages/forms/src/reducer.ts
+++ packages/forms/src/reducer.ts
@@ -17,6 +17,14 @@ import { reducerDictionary } from './utils';
 import { IFormState } from './interfaces';
 
 
+const initialState: IFormState = {
+  isNew: true,
+  source: {},
+  model: {},
+  errors: {},
+  isChanged: false,
+};
+
 const behaviors: Record<FormActionType, Function> = {
   [XCRITICAL_FORM_INIT]: (_state: IFormState, {
     payload,
@@ -69,13 +77,7 @@ const behaviors: Record<FormActionType, Function> = {
   }),
 };
 
-const reducer: Reducer<IFormState> = (state = {
-  isNew: true,
-  source: {},
-  model: {},
-  errors: {},
-  isChanged: false,
-}, action) => {
+const reducer: Reducer<IFormState> = (state = initialState, action) => {
   const behavior = behaviors[action.type];
 
   return behavior ? behavior(state, action) : state;
@@ -83,12 +85,12 @@ const reducer: Reducer<IFormState> = (state = {
 
 export const formSelector = (state: any, formName: string, namespace?: string): any => {
   if (!formName) {
-    return state;
+    return initialState;
   }
 
   return namespace
-    ? get(state, `${namespace}.${formName}`, state)
-    : get(state.form, formName, state);
+    ? get(state, `${namespace}.${formName}`, initialState)
+    : get(state.form, formName, initialState);
 };
 
 export default reducerDictionary(reducer, 'formName');
