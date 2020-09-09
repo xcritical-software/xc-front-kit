d320c6a5f099ecc0b6a6a1a4d96475960cf995a3
Artem Ilchenko <artemil@wizardsdev.com>
2020-09-09 09:32:20 +0300

    chore(switch): add new MyAccount switch example

--- packages/switch/stories/Switch.stories.tsx
+++ packages/switch/stories/Switch.stories.tsx
@@ -39,13 +39,11 @@ const theme = {
       },
     },
     mini: {
-      label: {
-        height: 4,
-      },
       container: {
         backgroundColor: 'rgb(50 67 78)',
         borderRadius: 5,
         width: 20,
+        height: 4,
         checked: {
           backgroundColor: 'rgb(64 172 173 / 25%)',
         },
@@ -64,6 +62,40 @@ const theme = {
         backgroundColor: 'rgb(121 131 138)',
         checked: {
           backgroundColor: 'rgb(64, 172, 173)',
+        },
+      },
+    },
+    myAccount: {
+      container: {
+        margin: '0 9px',
+        backgroundColor: 'rgb(161 161 161 / 35%)',
+        borderRadius: 100,
+        width: 19,
+        height: 8,
+        disabled: {
+          opacity: 0.4,
+        },
+        checked: {
+
+          backgroundColor: 'rgb(45 148 243 / 25%)',
+        },
+      },
+      handle: {
+        width: 18,
+        height: 18,
+        top: 0,
+        bottom: 0,
+        left: 0,
+        transform: 'translateX(-50%)',
+        checked: {
+          left: '100%',
+          transform: 'translateX(-50%)',
+        },
+      },
+      handleItem: {
+        backgroundColor: 'rgb(161 161 161)',
+        checked: {
+          backgroundColor: 'rgb(44 143 238)',
         },
       },
     },
@@ -93,6 +125,14 @@ storiesOf('Switch', module)
       <BasicSwitch
         theme={ { [switchThemeNamespace]: theme } }
         appearance="mini"
+        disabled={ boolean('Disabled', false) }
+        label={ text('Label', 'Test Label') }
+        labelPosition={ select('Label position', ['left', 'right'], 'right') }
+      />
+      <br />
+      <BasicSwitch
+        theme={ { [switchThemeNamespace]: theme } }
+        appearance="myAccount"
         disabled={ boolean('Disabled', false) }
         label={ text('Label', 'Test Label') }
         labelPosition={ select('Label position', ['left', 'right'], 'right') }
