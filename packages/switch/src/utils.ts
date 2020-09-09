08f816cb609d6bf914942ddc56313754ff3fd3f5
Artem Ilchenko <artemil@wizardsdev.com>
2020-09-09 13:11:38 +0300

    chore(switch): update return type for switch utils func

--- packages/switch/src/utils.ts
+++ packages/switch/src/utils.ts
@@ -1,8 +1,8 @@
 import memoize from 'micro-memoize';
 
-import { IThemeNamespace, getThemedState } from '@xcritical/theme';
+import { IThemeNamespace, getThemedState, ITheme } from '@xcritical/theme';
 
-import { AllType, getAppearanceTheme } from '@xcritical/theme/src';
+import { getAppearanceTheme } from '@xcritical/theme/src';
 
 import { ISwitchTheme } from './interfaces';
 import { switchThemeNamespace, defaultSwitchTheme } from './theme';
@@ -11,7 +11,7 @@ import { switchThemeNamespace, defaultSwitchTheme } from './theme';
 export const getSwitchStyles = memoize((
   theme?: IThemeNamespace<ISwitchTheme>,
   propertyPath?: string[],
-): any => {
+): ITheme<ISwitchTheme> => {
   const func = getThemedState(switchThemeNamespace, defaultSwitchTheme);
 
   return func(theme, propertyPath);
@@ -22,7 +22,7 @@ export const switchAppearanceTheme = (
   appearanceName: string,
   baseAppearance: string,
   propertyPath?: string | string[],
-): AllType => {
+): ITheme<ISwitchTheme> => {
   const func = getAppearanceTheme(switchThemeNamespace, defaultSwitchTheme);
 
   return func(theme, appearanceName, propertyPath, baseAppearance);
