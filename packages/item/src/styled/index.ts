e21fdcdb346eb97e75be769e410410ba2cc27fa4
Olga M <hempy.scrymetto@gmail.com>
2020-12-24 13:08:09 +0300

    feat(item): add cursor property in theme

--- packages/item/src/styled/index.ts
+++ packages/item/src/styled/index.ts
@@ -13,7 +13,7 @@ import { IPrefixPostfixProps, IContentProps, IRootProps } from '../interfaces';
 export const Root = styled.div<IRootProps>`
   ${getBaseStyle}
   box-sizing: border-box;
-  cursor: pointer;
+  cursor: ${({ theme, appearance, baseAppearance }): string => itemAppearanceTheme(theme, appearance, baseAppearance, 'cursor')};
   display: ${({ isHidden }): string => (isHidden ? 'none' : 'flex')};
   border-radius: ${({ theme, appearance, baseAppearance }): string => itemAppearanceTheme(theme, appearance, baseAppearance, 'borderRadius')}px;
   flex: none;
