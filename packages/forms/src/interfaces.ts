78377f39c7f42a05fe9002e5087e44e41e470a6a
Mikhail Andreev <tundraray@outlook.com>
2021-02-16 18:28:59 +0300

    feat(forms): remove forwardRef and add innerRef for proxying ref

--- packages/forms/src/interfaces.ts
+++ packages/forms/src/interfaces.ts
@@ -30,6 +30,7 @@ export interface IFormStateMap {
 export type FormFieldProps<TComponentProps> = TComponentProps & {
   component: React.ComponentType<TComponentProps & IFormFieldComponentProps>;
   name: string;
+  innerRef?: any;
   onChange?: IFormFieldComponentProps['onChange'];
 };
 
