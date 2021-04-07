a6aaa634fd172fef3591146382ba912b4444c6ab
Sergey Bondar <sergeyb@xcritical.software>
2021-04-07 19:02:04 +0300

    feat(item): Expand theme, rename styled components

--- packages/item/src/theme.ts
+++ packages/item/src/theme.ts
@@ -17,6 +17,22 @@ export const itemThemeStyle: ItemTheme = {
   borderRadius: 0,
   font: { weight: 'normal' },
   cursor: 'pointer',
+  contentWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    flexGrow: '1',
+    margin: '0',
+    overflow: 'hidden',
+    '&:first-child': {
+      margin: '0',
+    },
+  },
+  content: {
+    display: 'block',
+    flex: '1 1 auto',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+  },
   divided: {
     color: '#F0F0F0',
   },
