cf7f64a3404210b671e937a86c13f1bba07c7e2d
julia sirotkina <iulechkasirotkina@gmail.com>
2022-10-24 22:38:11 +0300

    chore(button): fix linter

--- packages/button/src/Button.tsx
+++ packages/button/src/Button.tsx
@@ -14,107 +14,108 @@ import {
 import { IButtonProps, ButtonTheme } from './interfaces';
 import { getElement } from './utils';
 
-export const PureButton = React.forwardRef<HTMLButtonElement, IButtonProps>(({
-  prefix,
-  postfix,
-  children,
-  href,
-  theme,
-  type = 'button',
-  role = 'button',
-  textPosition = 'center',
-  appearance = 'default',
-  baseAppearance = 'default',
-  spacing = 'default',
-  ghost = false,
-  disabled = false,
-  selected = false,
-  isRTL = false,
-  shouldFitContent = false,
-  component: CustomComponent,
-  onClick: onClickProps,
-  className,
-  classNamePrefix,
-  ...rest
-},
-ref: React.MutableRefObject<HTMLButtonElement>
-) => {
-  const themeContext = useContext<IThemeNamespace<ButtonTheme>>(ThemeContext);
-  const innerTheme = (theme ?? themeContext) || {};
-  const innerRef = useRef<HTMLButtonElement>(null);
-  const combinedRef = useCombinedRefs(null, ref, innerRef);
-
-
-  const onClick = useCallback(
-    (e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => {
-      if (onClickProps && !disabled) {
-        onClickProps(e);
-      }
+export const PureButton = React.forwardRef<HTMLButtonElement, IButtonProps>(
+  (
+    {
+      prefix,
+      postfix,
+      children,
+      href,
+      theme,
+      type = 'button',
+      role = 'button',
+      textPosition = 'center',
+      appearance = 'default',
+      baseAppearance = 'default',
+      spacing = 'default',
+      ghost = false,
+      disabled = false,
+      selected = false,
+      isRTL = false,
+      shouldFitContent = false,
+      component: CustomComponent,
+      onClick: onClickProps,
+      className,
+      classNamePrefix,
+      ...rest
     },
-    [disabled, onClickProps]
-  );
+    ref: React.MutableRefObject<HTMLButtonElement>
+  ) => {
+    const themeContext = useContext<IThemeNamespace<ButtonTheme>>(ThemeContext);
+    const innerTheme = (theme ?? themeContext) || {};
+    const innerRef = useRef<HTMLButtonElement>(null);
+    const combinedRef = useCombinedRefs(null, ref, innerRef);
 
-  const element = useMemo(
-    () => CustomComponent ?? getElement(disabled, href),
-    [CustomComponent, disabled, href]
-  );
+    const onClick = useCallback(
+      (e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => {
+        if (onClickProps && !disabled) {
+          onClickProps(e);
+        }
+      },
+      [disabled, onClickProps]
+    );
 
-  return (
-    <ThemeProvider theme={innerTheme}>
-      <StyledButton
-        className={className}
-        as={element}
-        ref={combinedRef}
-        role={role}
-        spacing={spacing}
-        isRTL={isRTL}
-        ghost={ghost}
-        disabled={disabled}
-        selected={selected}
-        shouldFitContent={shouldFitContent}
-        baseAppearance={baseAppearance}
-        appearance={appearance}
-        onClick={onClick}
-        type={type}
-        {...(href ? { href } : null)}
-        {...rest}>
-        {!!prefix && (
-          <Prefix
-            className={classNamePrefix && `${classNamePrefix}__prefix`}
-            isRTL={isRTL}
-            appearance={appearance}
-            baseAppearance={baseAppearance}>
-            {prefix}
-          </Prefix>
-        )}
+    const element = useMemo(
+      () => CustomComponent ?? getElement(disabled, href),
+      [CustomComponent, disabled, href]
+    );
 
-        <ContentWrapper
-          className={classNamePrefix && `${classNamePrefix}__content-wrapper`}
+    return (
+      <ThemeProvider theme={innerTheme}>
+        <StyledButton
+          className={className}
+          as={element}
+          ref={combinedRef}
+          role={role}
+          spacing={spacing}
+          isRTL={isRTL}
+          ghost={ghost}
+          disabled={disabled}
+          selected={selected}
+          shouldFitContent={shouldFitContent}
+          baseAppearance={baseAppearance}
           appearance={appearance}
-          baseAppearance={baseAppearance}>
-          <Content
-            className={classNamePrefix && `${classNamePrefix}__content`}
-            textPosition={textPosition}
-            isRTL={isRTL}
-            appearance={appearance}
-            baseAppearance={baseAppearance}>
-            {children}
-          </Content>
-        </ContentWrapper>
+          onClick={onClick}
+          type={type}
+          {...(href ? { href } : null)}
+          {...rest}>
+          {!!prefix && (
+            <Prefix
+              className={classNamePrefix && `${classNamePrefix}__prefix`}
+              isRTL={isRTL}
+              appearance={appearance}
+              baseAppearance={baseAppearance}>
+              {prefix}
+            </Prefix>
+          )}
 
-        {!!postfix && (
-          <Postfix
-            className={classNamePrefix && `${classNamePrefix}__postfix`}
-            isRTL={isRTL}
+          <ContentWrapper
+            className={classNamePrefix && `${classNamePrefix}__content-wrapper`}
             appearance={appearance}
             baseAppearance={baseAppearance}>
-            {postfix}
-          </Postfix>
-        )}
-      </StyledButton>
-    </ThemeProvider>
-  );
-}
+            <Content
+              className={classNamePrefix && `${classNamePrefix}__content`}
+              textPosition={textPosition}
+              isRTL={isRTL}
+              appearance={appearance}
+              baseAppearance={baseAppearance}>
+              {children}
+            </Content>
+          </ContentWrapper>
+
+          {!!postfix && (
+            <Postfix
+              className={classNamePrefix && `${classNamePrefix}__postfix`}
+              isRTL={isRTL}
+              appearance={appearance}
+              baseAppearance={baseAppearance}>
+              {postfix}
+            </Postfix>
+          )}
+        </StyledButton>
+      </ThemeProvider>
+    );
+  }
 );
 
 export const Button = memo(PureButton);
