ea403617233079e98db936a1ebb32cf0dc026256
Mikhail Andreev <tundraray@outlook.com>
2024-04-05 03:02:40 +0300

    fix(button): types

--- packages/button/src/ButtonGroup.tsx
+++ packages/button/src/ButtonGroup.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 
 import { StyledButtonGroup } from './styled';
 import { IButtonGroup } from './interfaces';
 
-export const ButtonGroup: React.FC<IButtonGroup> = ({
+export const ButtonGroup: React.FC<IButtonGroup & PropsWithChildren> = ({
   children,
   theme,
   className,
